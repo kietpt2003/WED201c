8379006fcfc959fab8a326be1ca8cc9c52e4158b
kietpt2003 <100004797+kietpt2003@users.noreply.github.com>
2023-05-04 10:04:41 +0700

    Delete Đề 2 (Đã giải)/WED201c_RE_FALL21_426789/PaperNo_2/All/Resource directory

--- Đề 2 (Đã giải)/WED201c_RE_FALL21_426789/PaperNo_2/All/Resource/js/mycode.js
+++
@@ -1,6 +0,0 @@
-function customerBuy(){
-    var phone = prompt('Please enter your phone number: ');
-    if(phone.length !=0 && parseInt(phone) == phone){
-        alert('We will contact to your number: ' + phone +' after 30 minutes')
-    }
-}
